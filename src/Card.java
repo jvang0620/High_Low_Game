2deed424738e89d36f4277d3d057b134f7537b5b
Jai_Vang <jaivang0387@gmail.com>
2024-10-03 22:31:59 -0400

    fixing card class

--- src/Card.java
+++ src/Card.java
@@ -1,40 +1,38 @@
 public class Card {
 
-    // instance variables (Part 1 Step 5)
     private int value;
     private String suit;
 
-    // constructor (Part 1 Step 6-8)
     public Card(int val, String sut) {
         this.value = val;
         this.suit = sut;
     }
 
-    // Step 9 (Part 1 add getter)
     public String getSuit() {
         return this.suit;
     }
 
-    // Step 9 (Part 1 last part) //Lab 12 step 6
+    // Overload the getValue method to work without any parameter
+    public int getValue() {
+        return this.value;
+    }
+
     public int getValue(boolean isBlackjack) {
         if (!isBlackjack) {
             return value;
         } else {
             if (value == 1) {
                 return 11;
+            } else if (value > 10) {
+                return 10;
             } else {
-                if (value > 10) {
-                    return 10;
-                } else {
-                    return value;
-                }
+                return value;
             }
         }
     }
 
-    // Part 1 Step 10
     public String declareCard() {
-        String result = null;
+        String result;
         switch (this.value) {
             case 11:
                 result = "the Jack of " + this.suit;
@@ -47,10 +45,10 @@ public class Card {
                 break;
             case 1:
                 result = "the Ace of " + this.suit;
+                break;
             default:
                 result = "the " + this.value + " of " + this.suit;
         }
         return result;
     }
-
 }
